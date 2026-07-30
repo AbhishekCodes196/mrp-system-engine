d1626663d40564d1214f08b96fc86d324d64e090
Abhishek Ahire <128769603+AbhishekCodes196@users.noreply.github.com>
2026-07-30 23:09:16 +0530

    Update PartRepository.java

--- PartRepository.java
+++ PartRepository.java
@@ -2,8 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface PartRepository extends JpaRepository<Part, Long> {
-     List<Part> findAllByOrderByIdAsc();
+    
+    // Fetches all parts automatically ordered by primary key ID
+    List<Part> findAllByOrderByIdAsc();
 }
