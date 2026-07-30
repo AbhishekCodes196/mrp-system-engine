8d9f025afd7b8a9d3d397054cc335946117100d7
Abhishek Ahire <128769603+AbhishekCodes196@users.noreply.github.com>
2026-07-30 22:58:26 +0530

    Refactor MrpResult class to use 'id' instead of 'partId'

--- MrpResult.java
+++ MrpResult.java
@@ -1,16 +1,18 @@
 package com.example.demo.model;
 
 public class MrpResult {
-    private Long partId;
+    private Long id;
     private String partName;
     private Integer grossRequirement;
     private Integer currentStock;
     private Integer netRequirement;
     private Integer leadTimeDays;
 
-    public MrpResult(Long partId, String partName, Integer grossRequirement, 
+    public MrpResult() {}
+
+    public MrpResult(Long id, String partName, Integer grossRequirement, 
                      Integer currentStock, Integer netRequirement, Integer leadTimeDays) {
-        this.partId = partId;
+        this.id = id;
         this.partName = partName;
         this.grossRequirement = grossRequirement;
         this.currentStock = currentStock;
@@ -18,10 +20,21 @@ public class MrpResult {
         this.leadTimeDays = leadTimeDays;
     }
 
-    public Long getPartId() { return partId; }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
     public String getPartName() { return partName; }
+    public void setPartName(String partName) { this.partName = partName; }
+
     public Integer getGrossRequirement() { return grossRequirement; }
+    public void setGrossRequirement(Integer grossRequirement) { this.grossRequirement = grossRequirement; }
+
     public Integer getCurrentStock() { return currentStock; }
+    public void setCurrentStock(Integer currentStock) { this.currentStock = currentStock; }
+
     public Integer getNetRequirement() { return netRequirement; }
+    public void setNetRequirement(Integer netRequirement) { this.netRequirement = netRequirement; }
+
     public Integer getLeadTimeDays() { return leadTimeDays; }
+    public void setLeadTimeDays(Integer leadTimeDays) { this.leadTimeDays = leadTimeDays; }
 }
